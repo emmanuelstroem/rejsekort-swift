47fbd6acaeb63a11572c7ee1c9930427bcecb5cc
Emmanuel <1090777+emmanuelstroem@users.noreply.github.com>
2022-06-05 21:09:24 +0200

    initial commit
    Signed-off-by: Emmanuel <1090777+emmanuelstroem@users.noreply.github.com>

---
+++ Shared/rejseprofilApp.swift
@@ -0,0 +1,17 @@
+//
+//  rejseprofilApp.swift
+//  Shared
+//
+//  Created by Emmanuel on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct rejseprofilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
